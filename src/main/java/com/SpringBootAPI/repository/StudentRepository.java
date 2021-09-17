2630f97573dc48acb79909f3070032b039dd76ef
Development <70835318+Development707@users.noreply.github.com>
2021-09-17 08:53:09 +0700

    start

--- src/main/java/com/SpringBootAPI/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.SpringBootAPI.repository;
-
-import com.SpringBootAPI.entity.Student;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StudentRepository extends CrudRepository<Student, Long> {
-}
